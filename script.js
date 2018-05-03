8b797a6062dd6bc3bd4c570c1fcf28b31dd74c14
Pawel Suchocki <pawel.suchocki212@gmail.com>
2018-05-03 22:01:48 +0200

    fix if problem

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getTriangleArea(a, h) {
-    if (return a > 0 && h > 0) {
-	console.log(return a * h / 2);
+    if (a > 0 && h > 0) {
+	  return (a * h / 2);
 } else {
 	console.log("nieprawidłowe dane");
 }
